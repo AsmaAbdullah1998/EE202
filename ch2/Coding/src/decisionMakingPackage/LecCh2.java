5b83b657be1bc82cc0f7681e809c33307ce29a2d
AsmaAbdullah1998 <asarouji0002@stu.kau.edu.sa>
2020-09-04 16:28:45 +0300

    .

--- ch2/Coding/src/decisionMakingPackage/LecCh2.java
+++ ch2/Coding/src/decisionMakingPackage/LecCh2.java
@@ -1,5 +1,9 @@
 package decisionMakingPackage;
 // if I used a default package I do not need to write a package name in the beginning of the programm
+import java.util.Scanner; //java.util--package, Scanner--class
+import java.lang.System;//if I write this or not it will not happen anything 
+//I have to import before the class
+
 public class LecCh2 {//CLASS
 
 	public static void main(String[] args) { //METHOD
@@ -19,10 +23,15 @@ public class LecCh2 {//CLASS
 		//void- means will not return any data or info
 		//main- the name of the method
 		
-		System.out.println("Hi there");
+		System.out.println("Welcome to Java Programming!");
 		//System --class
 		//System.out--object
 		//System.out.println--method
+		//() - anything inside the () is called "argument"
+		
+		System.out.printf("Welcome %s Java %s", "to", "programming!");
+		//%s- a place holder
+		//double stores a large than float
 		
 		/*
 		 * Steps for running a java program in cmd
@@ -30,6 +39,26 @@ public class LecCh2 {//CLASS
 		 ** from src file type "java <name of the package>.<name of the Class>"
 		 */
 		
+		/* To use Class inside anothor class
+		 ** import the class
+		 ** create object 
+		 */
+		
+		Scanner input = new Scanner(System.in);
+		//Scanner --the name of the class
+		//input--the name of the object
+		//new--create an object
+		//Scanner--the class
+		//System.in--standard input stream to let the user write data inside
+		
+		int number1;//type name of var;
+		int number2; 
+		int sum;
+		
+		System.out.print("Enter the first integer: "); //prompt--ask the user to enter the data
+		number1 = input.nextInt();//input-object, input.--after dot it will appear all methods 
+		
+		//Why I create object from a specific class? to use some methods inside the class
 		
 		
 		
