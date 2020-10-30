b13787c2ef51cab7eaa4148bcf4fa0ea23796478
AsmaAbdullah1998 <asarouji0002@stu.kau.edu.sa>
2020-10-30 21:39:04 +0300

    ..

--- ch6/src/Rolling.java
+++ ch6/src/Rolling.java
@@ -1,28 +1,27 @@
-import java.util.Scanner;
 import java.security.SecureRandom;
-public class CH6{
+public class Rolling {
 	static SecureRandom randomNumber = new SecureRandom();
-	static Scanner input = new Scanner(System.in);
+	
 	public static int rolling() {
-		int number1 = 1+ randomNumber.nextInt(6);
-		int number2 = 1+ randomNumber.nextInt(6);
+		int number1 = 1 + randomNumber.nextInt(6);
+		int number2 = 1 + randomNumber.nextInt(6);
+		
 		System.out.printf("%d+%d=%d", number1, number2, (number1+number2));
-		return number1+number2;
+		return (number1+number2);
 	}
+	
 	public static void main(String[] args) {
+		
 		int counter = 0;
 		while(true) {
 			int sum = rolling();
-			counter ++;
+			counter++;
 			if(sum == 7) {
-				System.out.printf(" You won after %d tries!", counter );
+				System.out.printf(" You won after %d tries!", counter);
 				break;
 			}
 			System.out.println();
 		}
-		
-		
-		
-	}//end main method
-}//end class
+	}
 
+}
