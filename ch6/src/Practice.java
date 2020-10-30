4e274a5f90f3388d26e3beda9c70296148c01f7b
AsmaAbdullah1998 <asarouji0002@stu.kau.edu.sa>
2020-10-30 11:32:41 +0300

    ch6

--- ch6/src/Practice.java
+++ ch6/src/Practice.java
@@ -1,22 +1,16 @@
-import java.util.*;
-import java.security.*;
+
+
 public class Practice {
 
-	private static boolean isTrue;
-	public static void main(String[] args) {
-		SecureRandom randomNumbers = new SecureRandom();
-		Scanner input = new Scanner(System.in);
-		int x = 11, y = 9;
-		if(x < 10)
-		if(x > 10)
-			System.out.println("**");
-		else
-			System.out.println("####");
-		System.out.println("$$$$");
-		
-		//System.out.println(randomNumbers.nextInt(3));
-		
-
+	private String name;
+	
+	public String getName( Practice.name) {
+		return name;
 	}
+	
+	public static void main(String[] args) {
+		
+	}//end 
+	
 
 }
