d5c29fa3df6541011fc2aabb7a37418c7d2cfc18
AsmaAbdullah1998 <asarouji0002@stu.kau.edu.sa>
2020-11-01 14:09:21 +0300

    ..

--- ch6/src/IsPrime.java
+++ ch6/src/IsPrime.java
@@ -5,9 +5,9 @@ public class IsPrime {
 
 	public static boolean isPrime(int number) {
 		boolean isPrime = true;
-		if(number == 1) isPrime = false;
+		if(number == 1 || number == 0) isPrime = false;
+		if(number % i == 0) {
 		for(int i = 2; i<=number-1; i++) {
-			if(number % i == 0) {
 				isPrime = false;
 				break;
 			}
@@ -16,10 +16,9 @@ public class IsPrime {
 	}
 	
 	public static void main(String[] args) {
-		System.out.print("Enter number: ");
-		int number = input.nextInt();
 		
-		System.out.println(isPrime(number)? "Prime":"NOT Prime");
+		
+		System.out.println(isPrime()? "Prime":"NOT Prime");
 	}
 
 }
